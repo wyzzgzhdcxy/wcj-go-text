7f651c98e605d802989f2c310452941566ee84de
wangchaojun <751849652@qq.com>
2026-08-26 19:09:05 +0800

    Sync: 2026-08-26 19:09:05

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ REM 用法：双击 build.bat 或在命令行执行 .\build.bat
 
 setlocal
 for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value ^| findstr /r "="') do set DATETIME=%%I
-set BUILD_TIME=%DATETIME:~0,4%-%DATETIME:~4,2%-%DATETIME:~6,2% %DATETIME:~8,2%:%DATETIME:~10,2%:%DATETIME:~12,2%
+set BUILD_TIME=%DATETIME:~0,4%-%DATETIME:~4,2%-%DATETIME:~6,2%T%DATETIME:~8,2%:%DATETIME:~10,2%:%DATETIME:~12,2%
 
 echo [build] BuildTime = %BUILD_TIME%
 wails build -ldflags "-X main.BuildTime=%BUILD_TIME%"
